43b66d652c7523fab36506b7fd5047aa9b446a48
An Bui <anhipchipchip@gmail.com>
2023-03-07 13:23:28 -0500

    testing route

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 //dependencies
-require("dotenv").config();
-
 const express = require('express')
-const cors = require('cors')
+require("dotenv").config();
+const app = express()
+
+// Connect MongoDB
 const dbConfig = require('./src/config/db.config')
 dbConfig.connectdb()
-const app = express()
+
+// use Cors
+const cors = require('cors')
+app.use(cors())
+
+// simple api to test
+app.get('/api/test', async (req, res) => {
+  try {
+    res.status(200).json('This is test api');
+  } catch (error) {
+    res.status(400).json(error);
+  }
+})
 
 app.use(express.json())
 
+// Run server
 const { PORT } = process.env
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
