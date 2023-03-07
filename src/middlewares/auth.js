6817c6d8af3790190edfe86fc0b4d43bc2261295
An Bui <anhipchipchip@gmail.com>
2023-03-07 15:27:56 -0500

    finish for verify token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,6 +3,16 @@ const jwt = require('jsonwebtoken')
 const {TOKEN_KEY} = process.env
 const verifyToken = (req, res, next) => {
     console.log('This is middleware for Auth')
-    next()
+    const token = req.body.token
+    if (!token) {
+        return res.status(400).send('You need to login')
+    }
+    try {
+        const decoded = jwt.verify(token, TOKEN_KEY)
+        req.user = decoded
+    } catch(err) {
+        return res.status(400).send('Token is not correct')
+    }
+    return next()
 }
 module.exports = verifyToken
