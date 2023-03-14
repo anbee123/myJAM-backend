98c5b38a227dc528ea33e8b39adc1542b7a92ea1
An Bui <anhipchipchip@gmail.com>
2023-03-13 21:45:43 -0400

    deploying

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,4 +2,5 @@ const controller = require('../controllers/auth.controller')
 module.exports = (app) => {
     app.post('/api/auth/signup', controller.signup)
     app.post('/api/auth/login', controller.login)
+    app.get('/api/auth/test')
 }
